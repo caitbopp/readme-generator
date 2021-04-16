f4b878c2a6297d7e803cf7449967a0a93dd54985
Caitlin Bopp <boppcaitlin@gmail.com>
2021-04-16 09:18:01 -0400

    updated generatemarkdown file, added in writefile function, added in inquirer.prompt

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+
+// Do I need to add inquirer.prompt {} here?
+
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please write a short description of your project:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'command',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        name: 'testCommand',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'userInfo',
+        message: 'What does the user need to know about using this repo?',
+    },
+    {
+        type: 'input',
+        name: 'userRepoInfo',
+        message: 'What does the user need to know about contributing to the repo?',
+    },
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// having a function that emncompasses fs.writeFile allows you to call function into action as many times as needed without writing fs peocess over and over
+function writeToFile(fileName, data) { 
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Success!')
+  );}
 
 // TODO: Create a function to initialize app
 function init() {
 
-    inquirer.prompt(questions).then(data); {
-
-    }
+    inquirer.prompt(questions).then((data) => {
+        writeToFile('README2.md', generateMarkdown(data))
+    })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `![mit](https://img.shields.io/badge/license-MIT-brightgreen)`
+  } else if (license === 'APACHE 2.0') {
+    return
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Same list as above but with license links
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return ``
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+// Same list as above but with license statements
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
+// add in readme template here
+
+
+
+## license
+${renderLicenseBadge(data.license)}
+${renderLicenseLink(data.license)}
+${renderLicenseSection(data.license)}
+
 
 `;
 }
